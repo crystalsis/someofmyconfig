55908d63ca2b7f3897463295712735216517b9c5
jakaz <wfz09317@163.com>
2018-12-20 18:00:21 +0800

    add

--- debian/init.sh
+++ debian/init.sh
@@ -13,10 +13,14 @@ echo 01 > ./demoCA/serial
 openssl genrsa -out ca.key 2048
 openssl req -new -key ca.key -out ca.csr
 #openssl x509 -req -days 36500 -in ca.csr -signkey ca.key -out ca.crt
-openssl req -new -x509 -days 36500 -key ca.key -out ca.crt subj "/C=CN/L=Qingdao/O=guagua/CN=consul.guagua.party"
+openssl req -new -x509 -days 36500 -key ca.key -out ca.crt subj "/C=CN/ST=Shandong/L=Qingdao/O=guagua/OU=party/CN=consul.guagua.party"
 openssl genrsa  -out server.key 2048
 openssl req -new -key server.key -out server.csr
 openssl ca -in server.csr -out server.crt -cert ca.crt -keyfile ca.key
+mkdir /etc/consul.d
+mkdir /etc/vault.d
+mkdir /etc/vault.d/data
+mkdir /etc/vault.d/plugins
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #curl -L https://get.oh-my.fish | fish
 #omf install lambda fonts mvn rvm
--- vault/init.hcl
+++ vault/init.hcl
@@ -2,6 +2,10 @@ storage "file" {
   path = "/etc/vault.d/data"
 }
 
+ha_storage "file" {
+  path = "/etc/vault.d/data"
+}
+
 listener "tcp" {
   address = "127.0.0.1:8200"
   cluster_address = "127.0.0.1:8201"
@@ -12,4 +16,21 @@ listener "tcp" {
   tls_disable = true
 }
 
+cluster_name = "v1"
+cache_size = "32000"
+disable_cache = false
+disable_mlock = false
+plugin_directory = "/etc/vault.d/plugins"
+telemetry {
+  disable_hostname = true
+}
+log_level = "INFO"
+default_lease_ttl = "768h"
+max_lease_ttl = "768h"
+default_max_request_duration = "90s"
+raw_storage_endpoint = false
 ui = true
+pid_file = "/var/run/vault.pid"
+api_addr = "http://127.0.0.1:8200"
+cluster_addr = "http://127.0.0.1:8201"
+disable_clustering = false
