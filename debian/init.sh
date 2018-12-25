d1514bc0d0aa7cee97118d53ac15004a3acc144d
jakaz <wfz09317@163.com>
2018-12-25 18:07:37 +0800

    compose

--- debian/init.sh
+++ debian/init.sh
@@ -9,6 +9,8 @@ apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt-get update
 apt-get install docker-ce
+curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 mkdir ./demoCA
 mkdir ./demoCA/certs
 mkdir ./demoCA/newcerts
