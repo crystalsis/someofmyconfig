0a09f78a4c0477fdad902bf42adbe8da9cd56311
jakaz <wfz09317@163.com>
2019-01-12 02:17:46 +0800

    mirrot

--- debian/init.sh
+++ debian/init.sh
@@ -1,23 +1,44 @@
 #bash -c "$(wget https://raw.githubusercontent.com/crystalsis/someofmyconfig/master/debian/init.sh -O -)"
 #su - root
 apt-get update
+apt-get install netselect-apt
+netselect-apt -s -n -c China -d -t 20 stretch
+curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+cat <<EOF >/etc/apt/sources.list
+deb https://mirrors.aliyun.com/debian/ stretch main non-free contrib
+deb-src https://mirrors.aliyun.com/debian/ stretch main non-free contrib
+deb https://mirrors.aliyun.com/debian-security stretch/updates main
+deb-src https://mirrors.aliyun.com/debian-security stretch/updates main
+deb https://mirrors.aliyun.com/debian/ stretch-updates main non-free contrib
+deb-src https://mirrors.aliyun.com/debian/ stretch-updates main non-free contrib
+deb https://mirrors.aliyun.com/debian/ stretch-backports main non-free contrib
+deb-src https://mirrors.aliyun.com/debian/ stretch-backports main non-free contrib
+deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/debian/ stretch stable
+deb-src [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/debian/ stretch stable
+EOF
 apt-get install -y apt-transport-https gnupg2 software-properties-common sudo vim-nox vim-pathogen vim-asciidoc vim-haproxy vim-youcompleteme vim-command-t zsh fish git curl wget jq busybox ipvsadm keepalived htop iotop iftop zip unzip liblxc1 strace socat dnsutils net-tools fonts-powerline lrzsz cmatrix cmatrix-xfont boxes libaa-bin sl toilet figlet bb fortune-mod fortune-zh cowsay lolcat screenfetch linuxlogo neofetch aview imagemagick pv espeak rig bastet ninvaders pacman4console nsnake greed bsdgames bsdgames moon-buggy pi ri fonts-noto fonts-ipafont-mincho fonts-ipafont-gothic fonts-arphic-ukai fonts-arphic-uming fonts-nanum fonts-mona fonts-vlgothic fonts-takao-gothic fonts-ipafont-gothic fonts-ipaexfont-gothic fonts-takao-mincho fonts-ipafont-mincho fonts-ipaexfont-mincho
 usermod -a -G sudo jakaz
 usermod -a -G adm jakaz
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+#docker
+apt-get remove docker docker-engine docker.io containerd runc
 apt-key fingerprint 0EBFCD88
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add -
+apt-get update
+apt-get install -y docker-ce
+#docker-conpose
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+#k8s
+curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add - 
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
+deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-stretch main
 EOF
 apt-get update
-apt-get install -y docker-ce kubelet kubeadm kubectl
-curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+apt-get install -y kubelet kubeadm kubectl
+#rkt
 wget https://github.com/rkt/rkt/releases/download/v1.30.0/rkt_1.30.0-1_amd64.deb
 dpkg -i rkt_1.30.0-1_amd64.deb
 rm -f rkt_1.30.0-1_amd64.deb
+#ca
 mkdir ./demoCA
 mkdir ./demoCA/certs
 mkdir ./demoCA/newcerts
@@ -39,21 +60,25 @@ mkdir /etc/vault.d/plugins
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #curl -L https://get.oh-my.fish | fish
 #omf install lambda fonts mvn rvm
+#consul
 wget https://releases.hashicorp.com/consul/1.4.0/consul_1.4.0_linux_amd64.zip
 unzip consul_1.4.0_linux_amd64.zip
 chmod +x consul
 mv consul /usr/bin/consul
 rm -f consul_1.4.0_linux_amd64.zip
+#vault
 wget https://releases.hashicorp.com/vault/1.0.0/vault_1.0.0_linux_amd64.zip
 unzip vault_1.0.0_linux_amd64.zip
 chmod +x vault
 mv vault /usr/bin/vault
 rm -f vault_1.0.0_linux_amd64.zip
+#nomad
 wget https://releases.hashicorp.com/nomad/0.8.6/nomad_0.8.6_linux_amd64-lxc.zip
 unzip nomad_0.8.6_linux_amd64-lxc.zip
 chmod +x nomad
 mv nomad /usr/bin/nomad
 rm -f nomad_0.8.6_linux_amd64-lxc.zip
+#fabio
 wget https://github.com/fabiolb/fabio/releases/download/v1.5.10/fabio-1.5.10-go1.11.1-linux_amd64
 chmod +x fabio-1.5.10-go1.11.1-linux_amd64
 mv fabio-1.5.10-go1.11.1-linux_amd64 /usr/bin/fabio
