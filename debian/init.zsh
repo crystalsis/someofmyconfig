af5bb58f607baf5650821f31ca2a123a0e594f9a
jakaz <wfz09317@163.com>
2019-01-18 21:03:17 +0800

    a

--- debian/init.zsh
+++ debian/init.zsh
@@ -6,10 +6,58 @@ curl -fsSL git.io/antigen > ~/.local/share/antigen.zsh
 curl -fsSL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 #zgen
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+#antibody
+curl -sL git.io/antibody | sh -s
+#awesome-fonts
+git clone https://github.com/gabrielelana/awesome-terminal-fonts.git
+mkdir -p ~/.fonts
+cp ~/awesome-terminal-fonts/build/*.* ~/.fonts/
+fc-cache -fv ~/.fonts
+mkdir -p ~/.config/fontconfig/conf.d
+cp ~/awesome-terminal-fonts/config/10-symbols.conf ~/.config/fontconfig/conf.d
 # .zshrc
 rm -f .zshrc
 cat <<EOF >~/.zshrc
 POWERLEVEL9K_INSTALLATION_PATH=$ANTIGEN_BUNDLES/bhilburn/powerlevel9k
+POWERLEVEL9K_MODE='awesome-fontconfig'
+POWERLEVEL9K_CONTEXT_TEMPLATE='%n'
+POWERLEVEL9K_CONTEXT_DEFAULT_FOREGROUND='white'
+POWERLEVEL9K_BATTERY_CHARGING='yellow'
+POWERLEVEL9K_BATTERY_CHARGED='green'
+POWERLEVEL9K_BATTERY_DISCONNECTED='$DEFAULT_COLOR'
+POWERLEVEL9K_BATTERY_LOW_THRESHOLD='10'
+POWERLEVEL9K_BATTERY_LOW_COLOR='red'
+POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=''
+POWERLEVEL9K_BATTERY_ICON='\uf1e6 '
+POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%F{014}\u2570%F{cyan}\uF460%F{073}\uF460%F{109}\uF460%f "
+POWERLEVEL9K_VCS_MODIFIED_BACKGROUND='yellow'
+POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND='yellow'
+POWERLEVEL9K_VCS_UNTRACKED_ICON='?'
+
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon context battery dir vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status time dir_writable ip custom_wifi_signal ram load background_jobs)
+
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
+
+POWERLEVEL9K_TIME_FORMAT="%D{\uf017 %H:%M:%S \uf073 %y/%m/%d}"
+POWERLEVEL9K_TIME_BACKGROUND='white'
+POWERLEVEL9K_RAM_BACKGROUND='yellow'
+POWERLEVEL9K_LOAD_CRITICAL_BACKGROUND="white"
+POWERLEVEL9K_LOAD_WARNING_BACKGROUND="white"
+POWERLEVEL9K_LOAD_NORMAL_BACKGROUND="white"
+POWERLEVEL9K_LOAD_CRITICAL_FOREGROUND="red"
+POWERLEVEL9K_LOAD_WARNING_FOREGROUND="yellow"
+POWERLEVEL9K_LOAD_NORMAL_FOREGROUND="black"
+POWERLEVEL9K_LOAD_CRITICAL_VISUAL_IDENTIFIER_COLOR="red"
+POWERLEVEL9K_LOAD_WARNING_VISUAL_IDENTIFIER_COLOR="yellow"
+POWERLEVEL9K_LOAD_NORMAL_VISUAL_IDENTIFIER_COLOR="green"
+POWERLEVEL9K_HOME_ICON=''
+POWERLEVEL9K_HOME_SUB_ICON=''
+POWERLEVEL9K_FOLDER_ICON=''
+POWERLEVEL9K_STATUS_VERBOSE=true
+POWERLEVEL9K_STATUS_CROSS=true
+
 export TERM="xterm-256color"
 export ZSH="/home/jakaz/.oh-my-zsh"
 export LANG=zh_CN.UTF-8
@@ -34,6 +82,18 @@ source ~/.zplug/init.zsh
 #zgen
 source "${HOME}/.zgen/zgen.zsh"
 
+plugins=(
+  apache2-macports
+  git
+  bundler
+  dotenv
+  osx
+  rake
+  rbenv
+  ruby
+  rails
+)
+
 # Load the oh-my-zsh's library.
 antigen use oh-my-zsh
 
@@ -53,7 +113,5 @@ antigen theme bhilburn/powerlevel9k powerlevel9k
 # Tell Antigen that you're done.
 antigen apply
 EOF
-#antibody
-curl -sL git.io/antibody | sh -s
 #oh-my-fish
 curl -L https://get.oh-my.fish | fish
